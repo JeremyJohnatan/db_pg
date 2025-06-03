6a38497f40132c3a9ddd9a9d129f6b68b39778e2
JeremyJohnatan <jeremy.johnatan@gmail.com>
2025-06-03 08:55:02 +0700

    progres terakhir

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class StaffController extends Controller
 {
@@ -19,6 +22,9 @@ class StaffController extends Controller
             'user' => $user,
             // Add more dashboard-specific data here
         ];
+
+
+        
 
         return view('dashboard.staff.dashboard', $dashboardData);
     }
